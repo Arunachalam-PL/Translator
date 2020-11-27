864f065d3572ab45a6b01303be902e91f114aba4
Arunachalam-PL <56231921+Arunachalam-PL@users.noreply.github.com>
2020-11-27 15:42:09 +0530

    Add files via upload

---
+++ example.py
@@ -0,0 +1,20 @@
+from googletrans import Translator
+import fileinput
+import time
+
+dict = {}
+
+try:
+    translator = Translator()
+    print(translator.translate("Hello",src='en',dest='fr').text)
+    f = open('newtext.txt','a')
+    for line in fileinput.input('find_words.txt'):
+        if(line != '\n'):
+            word = line.strip()
+            dict[word] = translator.translate(word,dest='fr').text
+            print(word+' - '+dict[word])
+            f.write(word+'-'+dict[word]+'\n')
+            time.sleep(0.5)
+    f.close()
+except:
+    print("This is VS Code") 
